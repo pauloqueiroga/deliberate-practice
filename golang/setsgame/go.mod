d11b42d233454712ca68694144f7c5b078a66e0a
Paulo Queiroga <890636+pauloqueiroga@users.noreply.github.com>
2021-09-09 16:00:33 -0700

    Added website parsing capabilities

--- golang/setsgame/go.mod
+++ golang/setsgame/go.mod
@@ -1,3 +1,5 @@
 module pauloqueiroga/setsgame
 
 go 1.17
+
+require golang.org/x/net v0.0.0-20210908191846-a5e095526f91 // indirect
--- golang/setsgame/main/main.go
+++ golang/setsgame/main/main.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"fmt"
-	"pauloqueiroga/setsgame"
+	"io/ioutil"
+	"net/http"
+
+	sg "pauloqueiroga/setsgame"
 )
 
 func main() {
-	board := setsgame.RandomBoard()
-	fmt.Println(board)
-	fmt.Println("===============")
-	sets := setsgame.BruteForceSolve(board)
-	fmt.Println(sets)
+	fmt.Println("Fetching today's puzzle...")
+	resp, err := http.Get("https://www.setgame.com/set/puzzle")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		fmt.Println("Got it. Reading HTML source...")
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			panic(err)
+		}
+
+		bodyString := string(bodyBytes)
+		fmt.Println("Read it. Parsing cards out of it...")
+		cards := sg.ParseCards(bodyString)
+		fmt.Println("Parsed. Creating board...")
+		board := sg.NewBoard(cards)
+		fmt.Printf("%+v\n", board)
+		fmt.Println("Solving Puzzle...")
+		sets := sg.BruteForceSolve(board)
+		fmt.Printf("%+v\n", sets)
+	}
 }
